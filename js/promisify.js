7456d417dc440b0fdc49ad241bdf538a95cf919d
aizigao <848200203@qq.com>
2019-08-10 10:24:19 +0800

    sync

---
+++ js/promisify.js
@@ -0,0 +1,14 @@
+// cb的方法 转 promise
+function promisify(original) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      args.push(function callback(err, ...values) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(...values);
+      });
+      original.call(this, ...args);
+    });
+  };
+}
