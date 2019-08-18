d52f1490b303cf142c12e6a8ae71e5e0e71cb869
aizigao <848200203@qq.com>
2019-08-18 18:19:53 +0800

    feat: restructuring

---
+++ basic_review/js/isPlainObject.js
@@ -0,0 +1,15 @@
+/***********************************
+ * 通过原型链 方式
+ **********************************/
+
+const isPlainObject = value => {
+  if (typeof value !== 'object' || value === null) return false;
+
+  let proto = value;
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  return Object.getPrototypeOf(value) === proto;
+};
+
