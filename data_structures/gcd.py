f93957d18e30669f7b2b1c048634fd407c90cb9e
aizigao <weixiangzhe@live.com>
2019-09-06 13:57:46 +0800

    sync

---
+++ data_structures/gcd.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+
+'''
+for findi
+g a greatest common divisor is Euclid’s Algorithm
+'''
+def gcd(m,n):
+    while m % n != 0:
+        m = n
+        n = m % oldn
+    return n
+
+print(gcd(20,10))
