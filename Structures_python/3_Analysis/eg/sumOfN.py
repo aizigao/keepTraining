0422bdf4909fa0c9bf59c28fe353bb639cf8d0e7
aizigao <weixiangzhe@live.com>
2020-03-23 12:59:19 +0800

    sync

---
+++ Structures_python/3_Analysis/eg/sumOfN.py
@@ -0,0 +1,18 @@
+import time;
+def sumOfN(n):
+    start_at = time.time()
+    the_sum = 0
+    for i in range(1, n+1):
+        the_sum += i
+    end_at = time.time()
+    return [the_sum, "sum is requred %10.7f seconds"%(end_at - start_at)]
+
+
+def sumOfN2(n):
+    start_at = time.time()
+    the_sum = (n*(n+1))/2
+    end_at = time.time()
+    return [the_sum, "sum is requred %10.7f seconds"%(end_at - start_at)]
+
+print(sumOfN(1000))
+print(sumOfN2(1000))
---
+++ Structures_python/3_Analysis/readme.md
@@ -0,0 +1,15 @@
+3.1. Objectives
+3.2. What Is Algorithm Analysis?
+3.3. Big-O Notation
+3.4. An Anagram Detection Example
+    3.4.1. Solution 1: Checking Off
+    3.4.2. Solution 2: Sort and Compare
+    3.4.3. Solution 3: Brute Force
+    3.4.4. Solution 4: Count and Compare
+3.5. Performance of Python Data Structures
+3.6. Lists
+3.7. Dictionaries
+3.8. Summary
+3.9. Key Terms
+3.10. Discussion Questions
+3.11. Programming Exercises
