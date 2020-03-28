4042e09fa0077c5c077382f4b0473161a86f384f
aizigao <weixiangzhe@live.com>
2020-03-28 17:24:41 +0800

    sync

---
+++ Structures_python/4_basic_data_structures/revstring_use_stack.py
@@ -0,0 +1,16 @@
+from pythonds.basic import Stack
+
+
+def revstring(orgStr):
+    tempStack = Stack()
+    resultStr = ''
+    # push item to stack
+    for s in list(orgStr):
+        tempStack.push(s)
+
+    while not tempStack.isEmpty():
+        resultStr += tempStack.pop()
+    return resultStr
+
+
+print(revstring('abcdef'))
