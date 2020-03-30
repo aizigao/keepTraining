df50b2e9ab688bd4c24d9e4e3f40e65bca389c77
aizigao <weixiangzhe@live.com>
2020-03-31 00:09:23 +0800

    sync

---
+++ Structures_python/4_basic_data_structures/lists/linkedList__unorderList.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, initData):
+        self.data = initData
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self, newdata):
+        self.data = newdata
+
+    def setNext(self, newnext):
+        self.next = newnext
+
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+
+        while current != None:
+            current = current.getNext()
+            count += 1
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+
+mylist = UnorderedList()
+mylist.add(31)
+mylist.add(77)
+mylist.add(88)
+mylist.add(31)
+print(mylist.size())
+print(mylist.search(31))
---
+++ Structures_python/4_basic_data_structures/lists/readme.md
@@ -0,0 +1,16 @@
+List
+
+- a collection of items where each item holds a relative postion with respect to the others;
+
+## unOrdered List Abstract Data Type
+
+- list()
+- add(item)
+- remove(item)
+- search(item)
+- isEmpty()
+- size()
+- append(item)
+- index(item)
+- pop()
+- pop(pos)
