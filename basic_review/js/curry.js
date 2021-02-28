fb7e8649749d6a0efebbde6b7a039cd9fa803d7d
aizigao <weixiangzhe@live.com>
2021-02-28 22:09:32 +0800

    sync

--- basic_review/js/curry.js
+++ basic_review/js/curry.js
@@ -1,53 +1,22 @@
-var add = (a, b) => a + b;
-
-// 版本一
-var curry = function(fn) {
-  var args = [].slice.call(arguments, 1);
-  return function() {
-    var newArgs = args.concat([].slice.call(arguments));
-    return fn.apply(this, newArgs);
-  };
-};
-
-var addCurry = curry(add, 1);
-
-var a = addCurry(2);
-
-console.log(a);
-
-// 第二版
-//
-function sub_curry(fn) {
-  var args = [].slice.call(arguments, 1);
-  return function() {
-    return fn.apply(
-      //--
-      this,
-      args.concat([].slice.call(arguments))
-    );
-  };
-}
-
-function curry1(fn, length) {
-  length = length || fn.length;
-
-  var slice = Array.prototype.slice;
-
-  return function() {
-    if (arguments.length < length) {
-      var combined = [fn].concat(slice.call(arguments));
-      return curry(sub_curry.apply(this, combined), length - arguments.length);
+// https://zh.javascript.info/currying-partials
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
     } else {
-      return fn.apply(this, arguments);
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
     }
   };
 }
 
-var fn = curry1(function(a, b, c) {
-  return [a, b, c];
-});
+function sum(a, b, c) {
+  return a + b + c;
+}
 
-fn("a", "b", "c"); // ["a", "b", "c"]
-fn("a", "b")("c"); // ["a", "b", "c"]
-fn("a")("b")("c"); // ["a", "b", "c"]
-fn("a")("b", "c"); // ["a", "b", "c"]
+let curriedSum = curry(sum);
+
+console.log(curriedSum(1, 2, 3)); // 6，仍然可以被正常调用
+console.log(curriedSum(1)(2, 3)); // 6，对第一个参数的柯里化
+console.log(curriedSum(1)(2)(3)); // 6，全柯里化
