a51948945f8a90fcf1358f2914ef8de39c58610a
aizigao <weixiangzhe@live.com>
2021-03-11 16:47:12 +0800

    leetcode

--- algorithm/leetCode_xx/22.括号生成.py
+++ algorithm/leetCode_xx/22.括号生成.py
@@ -13,7 +13,7 @@ class Solution:
     # 方法一：暴力法
     # 我们可以生成所有 2^ 2n 个 '(' 和 ')' 字符构成的序列，然后我们检查每一个是否有效即可。
     # O(2^2n * n) / O(n)
-    def generateParenthesis(self, n: int) -> List[str]:
+    def generateParenthesis_1(self, n: int) -> List[str]:
         ans = []
 
         def valid(A):
@@ -41,21 +41,21 @@ class Solution:
 
         generate([])
         return ans
-    # 方法二：回溯法
-
+    # 方法二：回溯法 , 和一差不太多，加了两个优化条件 TODO: 还是不太理解，用暴力的吧
     """
     方法一还有改进的余地：我们可以只在序列仍然保持有效时才添加 '(' or ')'，而不是像 方法一 那样每次添加。我们可以通过跟踪到目前为止放置的左括号和右括号的数目来做到这一点，
 
-    如果左括号数量不大于 nn，我们可以放一个左括号。如果右括号数量小于左括号的数量，我们可以放一个右括号。
+    如果左括号数量不大于 n，我们可以放一个左括号。如果右括号数量小于左括号的数量，我们可以放一个右括号。
     """
 
-    def generateParenthesis2(self, n: int) -> List[str]:
+    def generateParenthesis(self, n: int) -> List[str]:
         ans = []
 
         def backtrack(S, left, right):
             if len(S) == 2 * n:
                 ans.append(''.join(S))
                 return
+
             if left < n:
                 S.append('(')
                 backtrack(S, left+1, right)
