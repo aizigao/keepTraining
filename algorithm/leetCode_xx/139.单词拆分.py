caf82ac2dcbba951ebdca693f177a3d52f9fbb70
aizigao <weixiangzhe@live.com>
2021-07-28 16:52:53 +0800

    sync

--- algorithm/leetCode_xx/139.单词拆分.py
+++ algorithm/leetCode_xx/139.单词拆分.py
@@ -19,11 +19,11 @@ https://leetcode-cn.com/problems/word-break/solution/dan-ci-chai-fen-by-leetcode
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
-        dp = [False] * (n + 1)
-        dp[0] = True
+        dp = [True] + [False] * n
+
         for i in range(n):
             for j in range(i + 1, n + 1):
-                if (dp[i] and (s[i:j] in wordDict)):
+                if dp[i] and s[i:j] in wordDict:
                     dp[j] = True
         return dp[-1]
 
