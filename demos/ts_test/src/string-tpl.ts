7df63a1cae29c6a1f5f69514b9b99089f6b9558c
aizigao <weixiangzhe@live.com>
2021-07-28 13:31:49 +0800

    sync

--- demos/ts_test/src/string-tpl.ts
+++ demos/ts_test/src/string-tpl.ts
@@ -1,1 +1,20 @@
-// https://mp.weixin.qq.com/s/KJdUdwbLN4g4M7xy34m-fA
+type Flatten<T> = T extends unknown[] ? T[number] : T;
+
+// Extracts out the element type.
+type Str = Flatten<string[]>;
+//   string
+
+// Leaves the type alone.
+type Num = Flatten<number>;
+//   number
+
+interface Animal {
+  live(): void;
+}
+interface Dog extends Animal {
+  woof(): void;
+}
+
+type Example1 = Dog extends Animal ? number : string;
+
+type Example2 = RegExp extends Animal ? number : string;
