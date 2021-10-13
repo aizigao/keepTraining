4cd75f396706676732e9ff7bb7d3a077cf26a7fc
aizigao <weixiangzhe@live.com>
2021-10-13 22:22:50 +0800

    sync

--- algorithm/oi-wiki/basic/recursion_stack/hanoi.md
+++ algorithm/oi-wiki/basic/recursion_stack/hanoi.md
@@ -1,2 +1,4 @@
+https://www.bilibili.com/video/BV1Hx411U7bh?p=9
 ![](images/2021-10-13-21-37-27.png)
 ![](images/2021-10-13-21-41-47.png)
+![](images/2021-10-13-22-05-54.png)
--- algorithm/oi-wiki/basic/recursion_stack/hanoi.py
+++ algorithm/oi-wiki/basic/recursion_stack/hanoi.py
@@ -1,27 +1,27 @@
-def hanoi(n, src, mid, dist):
+def hanoi(n, src, mid, dest):
     # stack 存储子问题的参数
     stack = []
 
-    stack.append([n, src, mid, dist])
+    stack.append([n, src, mid, dest])
 
     while stack:
         subHanoi = stack.pop()
-        (s_n, s_src, s_mid, s_dist) = subHanoi
+        (s_n, s_src, s_mid, s_dest) = subHanoi
 
         if s_n == 1:
-            print('移动 %s --> %s' % (s_src, s_dist))
+            print('移动 %s --> %s' % (s_src, s_dest))
         else:
             '''
-            注意放置 顺序
+            注意放置 顺序, 最后的运算最放在 栈底的， 所以要先放入
             '''
             # 第三个子问题
-            stack.append((s_n - 1, s_mid, s_src, s_dist))
+            stack.append((s_n - 1, s_mid, s_src, s_dest))
 
             # 第二个子问题
-            stack.append((1, s_src, s_mid, s_dist))
+            stack.append((1, s_src, s_mid, s_dest))
 
             # 第一个子问题
-            stack.append((s_n - 1, s_src, s_dist, s_mid))
+            stack.append((s_n - 1, s_src, s_dest, s_mid))
 
 
 hanoi(3, 'A', 'B', 'C')
