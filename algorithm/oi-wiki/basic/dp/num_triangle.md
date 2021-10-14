e85e2b47408c7d9639b67eb0bd6bd3c59df9227a
aizigao <weixiangzhe@live.com>
2021-10-14 22:01:42 +0800

    sync

--- algorithm/oi-wiki/basic/dp/num_triangle.md
+++ algorithm/oi-wiki/basic/dp/num_triangle.md
@@ -6,3 +6,8 @@ https://www.bilibili.com/video/BV1Hx411U7bh?p=10
 ![](images/2021-10-14-21-15-03.png)
 ![](images/2021-10-14-21-20-48.png)
 ![](images/2021-10-14-21-41-23.png)
+
+
+人人为我 式递推
+
+![](images/2021-10-14-21-53-40.png)
--- algorithm/oi-wiki/basic/dp/num_triangle.py
+++ algorithm/oi-wiki/basic/dp/num_triangle.py
@@ -18,10 +18,30 @@ def maxSum(i, j):
     if i == rows - 1:
         return data[i][j]
     left = maxSum(i + 1, j)
-    right = maxSum(i + 1, j)
+    right = maxSum(i + 1, j + 1)
     cur_v = data[i][j] + max(left, right)
     dp[i][j] = cur_v
     return cur_v
 
+
 rst = maxSum(0, 0)
 print(rst)  # 30
+
+
+# ------------------------
+# 方法二
+# 递推 人人为我, 从底到上
+def maxSum2():
+    i = rows - 2
+    while i > -1:
+        for j in range(len(data[i])):
+            data[i][j] = data[i][j] + max(\
+                data[i+1][j],
+                data[i+1][j+1]
+            )
+            j += 1
+        i -= 1
+    return data[0][0]
+
+
+print(maxSum2())
