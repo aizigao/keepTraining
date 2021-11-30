086c11c0b5465cdb369cbe614d1e092063dddd56
aizigao <weixiangzhe@live.com>
2021-11-30 09:46:39 +0800

    feat: sync

--- algorithm/leetCode_xx/494.目标和.py
+++ algorithm/leetCode_xx/494.目标和.py
@@ -25,7 +25,7 @@ class Solution:
         return count
 
     # -- 方法二 动态规划
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    def findTargetSumWaysDP(self, nums: List[int], target: int) -> int:
         sum = 0
         for num in nums:
             sum += num
@@ -49,6 +49,27 @@ class Solution:
                 if j >= num:
                     dp[i][j] += dp[i - 1][j - num]
         return dp[n][neg]
+
+    # 由于 \textit{dp}dp 的每一行的计算只和上一行有关，因此可以使用滚动数组的方式，去掉 \textit{dp}dp 的第一个维度，将空间复杂度优化到 O(\textit{neg})O(neg)。
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        sum = 0
+        for num in nums:
+            sum += num
+
+        diff = sum - target
+
+        if diff < 0 or diff % 2 != 0:
+            return 0
+
+        n = len(nums)
+        neg = int(diff / 2)
+
+        dp = [0 for i in range(neg + 1)]
+        dp[0] = 1
+        for num in nums:
+            for j in range(neg, num - 1, -1):
+                dp[j] += dp[j - num]
+        return dp[neg]
 
 
 # @lc code=end
