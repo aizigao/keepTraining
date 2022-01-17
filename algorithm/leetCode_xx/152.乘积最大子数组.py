f5009f40f5da630e241a16ef0dc0ce9e1f668727
aizigao <weixiangzhe@live.com>
2022-01-17 10:34:21 +0800

    sync

--- algorithm/leetCode_xx/152.乘积最大子数组.py
+++ algorithm/leetCode_xx/152.乘积最大子数组.py
@@ -7,9 +7,22 @@
 
 # @lc code=start
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        # TODO:
-        pass
+    def maxProduct(self, nums):
+
+        #  int maxF = nums[0], minF = nums[0], ans = nums[0];
+        max_n = nums[0]
+        min_n = nums[0]
+        ans = nums[0]
+        size = len(nums)
+
+        for i in range(1, size):
+            mx = max_n
+            mn = min_n
+
+            max_n = max(mx * nums[i], nums[i], mn * nums[i])
+            min_n = min(mn * nums[i], nums[i], mx * nums[i])
+            ans = max(max_n, ans)
+        return ans
 
 
 # @lc code=end
