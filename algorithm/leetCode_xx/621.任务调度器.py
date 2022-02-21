ee6a451f691354b6bf54b620b45eca52ddfe2204
aizigao <weixiangzhe@live.com>
2022-02-21 14:35:59 +0800

    stash

--- algorithm/leetCode_xx/621.任务调度器.py
+++ algorithm/leetCode_xx/621.任务调度器.py
@@ -5,7 +5,36 @@
 #
 
 # @lc code=start
-class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-# @lc code=end
+import collections
 
+
+class Solution:
+    ## 模拟
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = collections.Counter(tasks)
+        # 任务类别总数
+        m = len(freq)
+
+        print(freq, tasks)
+        # 冷却限制, 最早可以执行时间
+        nextValid = [1] * m
+        # 剩余次数
+        rest = list(freq.values())
+        time = 0
+
+        for i in range(len(tasks)):
+            time += 1
+            minNextValid = min(nextValid[j] for j in range(m) if rest[j] > 0)
+            time = max(time, minNextValid)
+
+            # 找出最大次数那个 
+            best = -1
+            for j in range(m):
+                if rest[j] and nextValid[j] <= time:
+                    if best == -1 or nextValid[j] > rest[best]:
+                        best = j
+            nextValid[best] = time + n + 1
+        return time
+
+
+# @lc code=end
