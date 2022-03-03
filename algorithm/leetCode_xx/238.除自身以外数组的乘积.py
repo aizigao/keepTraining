aebfa6aff859eb08772ebf29900ea3208724a07f
aizigao <weixiangzhe@live.com>
2022-03-03 15:35:37 +0800

    sync

--- algorithm/leetCode_xx/238.除自身以外数组的乘积.py
+++ algorithm/leetCode_xx/238.除自身以外数组的乘积.py
@@ -29,6 +29,31 @@ class Solution:
             ans[i] = l[i] * r[i]
 
         return ans
+    # 方法二 O(1) 优化
+    '''
+    空间复杂度：O(1)O(1)，输出数组不算进空间复杂度中，因此我们只需要常数的空间存放变量。
+    '''
+    def productExceptSelf2(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        answer = [0]*length
+        
+        # answer[i] 表示索引 i 左侧所有元素的乘积
+        # 因为索引为 '0' 的元素左侧没有元素， 所以 answer[0] = 1
+        answer[0] = 1
+        for i in range(1, length):
+            answer[i] = nums[i - 1] * answer[i - 1]
+        
+        # R 为右侧所有元素的乘积
+        # 刚开始右边没有元素，所以 R = 1
+        R = 1;
+        for i in reversed(range(length)):
+            # 对于索引 i，左边的乘积为 answer[i]，右边的乘积为 R
+            answer[i] = answer[i] * R
+            # R 需要包含右边所有的乘积，所以计算下一个结果时需要将当前值乘到 R 上
+            R *= nums[i]
+        
+        return answer
+
 
 
 # @lc code=end
