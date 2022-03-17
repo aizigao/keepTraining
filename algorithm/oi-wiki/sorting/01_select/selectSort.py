8ba67f6917dea73f381a97dbc3091386133467d8
aizigao <weixiangzhe@live.com>
2022-03-17 15:35:55 +0800

    feat: sync

--- algorithm/oi-wiki/sorting/01_select/selectSort.py
+++ algorithm/oi-wiki/sorting/01_select/selectSort.py
@@ -2,17 +2,35 @@
 
 
 def selectionSort(alist):
-    for fillslot in range(len(alist)-1, 0, -1):
+    for fillslot in range(len(alist) - 1, 0, -1):
         positionOfMax = 0
-        for location in range(1, fillslot+1):
+        for location in range(1, fillslot + 1):
             if alist[location] > alist[positionOfMax]:
                 positionOfMax = location
 
         temp = alist[fillslot]
         alist[fillslot] = alist[positionOfMax]
         alist[positionOfMax] = temp
+    return alist
 
 
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-selectionSort(alist)
+
+
+def selectSort2(aList):
+    n = len(alist)
+    for i in range(n):
+        # find min of rest
+        min_of_rest_idx = i
+        for j in range(i, n):
+            if aList[j] < aList[min_of_rest_idx]:
+                min_of_rest_idx = j
+
+        # replace
+        aList[i], aList[min_of_rest_idx] = aList[min_of_rest_idx], aList[i]
+    return aList
+
+
+print(selectionSort(alist.copy()))
+print(selectSort2(alist.copy()))
 print(alist)
