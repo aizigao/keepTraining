eb81cd7cd2faf7fb34fbedfffedee0f23fac850b
aizigao <weixiangzhe@live.com>
2022-03-18 17:27:43 +0800

    feat: sync

--- algorithm/oi-wiki/basic/sorting/08_merge/merge.py
+++ algorithm/oi-wiki/basic/sorting/08_merge/merge.py
@@ -1,34 +1,33 @@
 def mergeSort(alist):
-    size = len(alist)
-    if size <= 1:
-        return alist
-    mid = size // 2
-    left_list = alist[:mid]
-    l_size = len(left_list)
-    right_list = alist[mid:]
-    r_size = len(right_list)
-
-    mergeSort(left_list)
-    mergeSort(right_list)
+    n = len(alist)
+    if n <= 1:
+        return
+    mid = n // 2
+    L = alist[:mid]
+    R = alist[mid:]
+    mergeSort(L)
+    mergeSort(R)
 
     l, r, k = 0, 0, 0
 
-    while l < l_size and r < r_size:
-        if left_list[l] <= right_list[r]:
-            alist[k] = left_list[l]
+    while l < len(L) and r < len(R):
+        if L[l] < R[r]:
+            alist[k] = L[l]
             l += 1
         else:
-            alist[k] = right_list[r]
+            alist[k] = R[r]
             r += 1
-        k += 1
-    while l < l_size:
-        alist[k] = left_list[l]
+        k + 1
+
+    while l < len(L):
+        alist[k] = L[l]
         l += 1
-        k += 1
-    while r < r_size:
-        alist[k] = right_list[r]
+        k + 1
+
+    while r < len(R):
+        alist[k] = R[r]
         r += 1
-        k += 1
+        k + 1
 
 
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
