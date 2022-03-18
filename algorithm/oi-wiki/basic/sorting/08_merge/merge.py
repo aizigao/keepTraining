89e31ea11555554fd8bc04ce37119f2e99b42e09
aizigao <weixiangzhe@live.com>
2022-03-18 17:30:22 +0800

    feat: sync

--- algorithm/oi-wiki/basic/sorting/08_merge/merge.py
+++ algorithm/oi-wiki/basic/sorting/08_merge/merge.py
@@ -2,14 +2,20 @@ def mergeSort(alist):
     n = len(alist)
     if n <= 1:
         return
+
+    # 拆成左右两组
     mid = n // 2
     L = alist[:mid]
     R = alist[mid:]
+
+    # 分别排序
     mergeSort(L)
     mergeSort(R)
 
     l, r, k = 0, 0, 0
 
+    # 合并
+    # 1. 左，右各取一个，小的放到结果中，然后 loop 
     while l < len(L) and r < len(R):
         if L[l] < R[r]:
             alist[k] = L[l]
@@ -19,6 +25,7 @@ def mergeSort(alist):
             r += 1
         k + 1
 
+    # 左，右后已经取完了，要把剩余的内容放到结果中
     while l < len(L):
         alist[k] = L[l]
         l += 1
