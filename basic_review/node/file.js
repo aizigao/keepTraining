798b90983efdaab938392170ce14a883e92b319c
aizigao <weixiangzhe@live.com>
2022-04-08 14:54:25 +0800

    feat: use pnpm

--- basic_review/node/file.js
+++ basic_review/node/file.js
@@ -1,20 +1,27 @@
 // touch write
 
 const fs = require("fs");
+const np = require("number-precision");
 
-const file =
-  "/Users/aizigao/MyWorkPlace/ibus/app/active/active_pages_umi/src/pages/CardList/build/index/_pages/CardList/component/show.css";
+const file = process.argv[2];
 
-fs.readFile(file, "utf-8", (err, data) => {
-  console.log(data);
-  // => [Error: EISDIR: illegal operation on a directory, read <directory>]
-  const rdata = data.replace(/([\d.]*)rem/g, (match, $1) => {
-    const r = Math.round(Number($1) / 0.02);
-    return r + "px";
-  });
-  fs.writeFile(file, rdata, (err) => {
-    if (err) {
-      console.log(err);
+const main = () => {
+    if (!file) {
+        return;
     }
-  });
-});
+    fs.readFile(file, "utf-8", (err, data) => {
+        console.log(data);
+        // => [Error: EISDIR: illegal operation on a directory, read <directory>]
+        const rdata = data.replace(/([\d.]*)px/g, (match, $1) => {
+            const r = np.times(Number($1), 0.01);
+            return r + "rem";
+        });
+        fs.writeFile(file, rdata, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+};
+
+main()
