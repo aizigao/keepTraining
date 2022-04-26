9a3e7d981b8e3ccb8f43d186586756bca32c8e8b
aizigao <weixiangzhe@live.com>
2022-04-26 13:56:08 +0800

    sync

--- algorithm/leetCode_xx/42.接雨水.py
+++ algorithm/leetCode_xx/42.接雨水.py
@@ -8,15 +8,26 @@
 # @lc code=start
 class Solution:
     '''
+    方法一: dp
+    '''
+    def trap(self, height: List[int]) -> int:
+        pass
+
+    '''
+    '''
+    '''
+    '''
+    '''
     方法三: 双指针
 
     下标 i 处能接的雨水由 leftMax[i] 与 rightMax[i] 的最小值决定
-
+    时：O(n)
+    空: O(1)
     '''
-    def trap(self, height: List[int]) -> int:
+
+    def trap3(self, height: List[int]) -> int:
         ans = 0
         left, right = 0, len(height) - 1
-
         leftMax, rightMax = 0, 0
 
         while left < right:
