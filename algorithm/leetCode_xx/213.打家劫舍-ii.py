b39de117de1af60d03018fa472544dee1a282e61
aizigao <weixiangzhe@live.com>
2022-05-09 17:11:31 +0800

    feat: sync

---
+++ algorithm/leetCode_xx/213.打家劫舍-ii.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode.cn id=213 lang=python3
+#
+# [213] 打家劫舍 II
+#
+
+# @lc code=start
+
+
+class Solution:
+    '''
+    如何才能保证第一间房屋和最后一间房屋不同时偷窃呢？如果偷窃了第一间房屋，则不能偷窃最后一间房屋，因此偷窃房屋的范围是第一间房屋到最后第二间房屋；如果偷窃了最后一间房屋，则不能偷窃第一间房屋，因此偷窃房屋的范围是第二间房屋到最后一间房屋
+    '''
+    def rob(self, nums: List[int]) -> int:
+
+        if not nums:
+            return 0
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        def robRange(start, end):
+            p = nums[start]
+            q = max(nums[start], nums[start + 1])
+
+            for i in range(start + 2, end + 1):
+                temp = max(p + nums[i], q)
+                p = q
+                q = temp
+            return q
+
+        '''
+        如果 偷窃了第一间房屋，则不能偷窃最后一间房
+        '''
+
+        return max(robRange(0, n - 2), robRange(1, n - 1))
+
+
+# @lc code=end
