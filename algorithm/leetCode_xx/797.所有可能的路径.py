0d9a9b2e2392b95d66186b1c6e4a96597191d844
aizigao <weixiangzhe@live.com>
2022-05-12 17:16:02 +0800

    sync

---
+++ algorithm/leetCode_xx/797.所有可能的路径.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode.cn id=797 lang=python3
+#
+# [797] 所有可能的路径
+#
+
+
+# @lc code=start
+class Solution:
+    # TODO: 官方的解法也要看一看
+    def traverse(self, graph, s, path, res):
+        path.append(s)
+        n = len(graph)
+        # 到终点
+        if s == n - 1:
+            print(path)
+            res.append(path[:])
+        for ss in graph[s]:
+            self.traverse(graph, ss, path, res)
+        path.pop()
+
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        res = []
+        path = []
+        self.traverse(graph, 0, path, res)
+        return res
+
+
+# @lc code=end
