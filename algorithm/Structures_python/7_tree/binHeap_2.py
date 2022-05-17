4c49be594d794b425044bcbd641020ddc94d87b7
aizigao <weixiangzhe@live.com>
2022-05-17 10:51:49 +0800

    sync

--- algorithm/Structures_python/7_tree/binHeap_2.py
+++ algorithm/Structures_python/7_tree/binHeap_2.py
@@ -3,62 +3,66 @@ The assertion that we can build the heap in 𝑂(𝑛) may seem a bit mysterious
 
 Using the fact that you can build a heap from a list in 𝑂(𝑛) time, you will construct a sorting algorithm that uses a heap and sorts a list in 𝑂(𝑛log𝑛)) as an exercise at the end of this chapter.
 '''
+'''
+小顶堆 
+'''
+
 
 class BinHeap:
     def __init__(self):
         self.heapList = [0]
         self.currentSize = 0
 
-
-    def percUp(self,i):
+    # 上移
+    def percUp(self, i):
         while i // 2 > 0:
-          if self.heapList[i] < self.heapList[i // 2]:
-             tmp = self.heapList[i // 2]
-             self.heapList[i // 2] = self.heapList[i]
-             self.heapList[i] = tmp
-          i = i // 2
+            if self.heapList[i] < self.heapList[i // 2]:
+                # swap
+                self.heapList[i // 2], self.heapList[i] = self.heapList[
+                    i], tmp = self.heapList[i // 2]
+            i = i // 2
 
-    def insert(self,k):
-      self.heapList.append(k)
-      self.currentSize = self.currentSize + 1
-      self.percUp(self.currentSize)
+    def insert(self, k):
+        self.heapList.append(k)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSze)
 
-    def percDown(self,i):
-      while (i * 2) <= self.currentSize:
-          mc = self.minChild(i)
-          if self.heapList[i] > self.heapList[mc]:
-              tmp = self.heapList[i]
-              self.heapList[i] = self.heapList[mc]
-              self.heapList[mc] = tmp
-          i = mc
+    def percDown(self, i):
+        while (i * 2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                self.heapList[i], self.heapList[mc] = self.heapList[
+                    mc], self.heapList[i]
+            i = mc
 
-    def minChild(self,i):
-      if i * 2 + 1 > self.currentSize:
-          return i * 2
-      else:
-          if self.heapList[i*2] < self.heapList[i*2+1]:
-              return i * 2
-          else:
-              return i * 2 + 1
+    def minChild(self, i):
+        if i * 2 + 1 > self.currentSize:
+            return i * 2
+        else:
+            if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
 
     def delMin(self):
-      retval = self.heapList[1]
-      self.heapList[1] = self.heapList[self.currentSize]
-      self.currentSize = self.currentSize - 1
-      self.heapList.pop()
-      self.percDown(1)
-      return retval
+        retval = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
 
-    def buildHeap(self,alist):
-      i = len(alist) // 2
-      self.currentSize = len(alist)
-      self.heapList = [0] + alist[:]
-      while (i > 0):
-          self.percDown(i)
-          i = i - 1
+    def buildHeap(self, alist):
+        i = len(alist) // 2
+        self.currentSize = len(alist)
+        self.heapList = [0] + alist[:]
+        while (i > 0):
+            self.percDown(i)
+            i = i - 1
+
 
 bh = BinHeap()
-bh.buildHeap([9,5,6,2,3])
+bh.buildHeap([9, 5, 6, 2, 3])
 
 print(bh.delMin())
 print(bh.delMin())
---
+++ algorithm/oi-wiki/basic/bin_heap/index.md
@@ -0,0 +1,3 @@
+https://oi-wiki.org/ds/binary-heap/
+
+![](images/2022-05-17-10-38-20.png)
