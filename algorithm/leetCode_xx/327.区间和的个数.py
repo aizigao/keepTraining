79bd9f680becba5469a5f7847bcd91c9dbfcf785
aizigao <weixiangzhe@live.com>
2022-05-24 17:48:42 +0800

    feat: sync

---
+++ algorithm/leetCode_xx/327.区间和的个数.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=327 lang=python3
+#
+# [327] 区间和的个数
+#
+
+# @lc code=start
+class Solution:
+    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+        pass
+# @lc code=end
+
