f66bb73266b280b6d51ebdbb4fa908d04570c52c
aizigao <weixiangzhe@live.com>
2022-06-13 14:21:08 +0800

    sync

--- algorithm/leetCode_xx/95.不同的二叉搜索树-ii.py
+++ algorithm/leetCode_xx/95.不同的二叉搜索树-ii.py
@@ -13,8 +13,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    '''
+    根节点值 大于左子树，小于右子树
+    '''
     def generateTrees(self, n: int) -> List[TreeNode]:
-        pass
+        def generateTrees(start, end):
+            # base
+            if start > end:
+                return [None]
+
+            allTrees = []
+            for i in range(start, end + 1):
+                leftTrees = generateTrees(start, i - 1)
+                rightTress = generateTrees(i + 1, end)
+
+                # 左树 和 右树各取一个 拼接
+                for l in leftTrees:
+                    for r in rightTress:
+                        currTree = TreeNode(i)
+                        currTree.left = l
+                        currTree.right = r
+                        allTrees.append(currTree)
+            return allTrees
+
+        return generateTrees(1, n) if n else []
 
 
 # @lc code=end
