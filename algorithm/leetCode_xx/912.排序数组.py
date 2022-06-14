12d8c44adc2d37199f5dca75b15f1b11e1818d71
aizigao <weixiangzhe@live.com>
2022-06-14 10:10:42 +0800

    feat: sync

--- algorithm/leetCode_xx/912.排序数组.py
+++ algorithm/leetCode_xx/912.排序数组.py
@@ -10,22 +10,24 @@
 class Solution:
     def randomized_partition(self, nums, l, r):
         pivot = random.randint(l, r)
-        nums[pivot], nums[r] = nums[r], nums[pivot]
+        nums[r], nums[pivot] = nums[pivot], nums[r]
+
         i = l - 1
         for j in range(l, r):
             if nums[j] < nums[r]:
                 i += 1
-                nums[j], nums[i] = nums[i], nums[j]
+                nums[i], nums[j] = nums[j], nums[i]
         i += 1
-        nums[i], nums[r] = nums[r], nums[i]
+        nums[r], nums[i] = nums[i], nums[r]
         return i
 
     def randomized_quicksort(self, nums, l, r):
         if l >= r:
             return
+
         mid = self.randomized_partition(nums, l, r)
-        self.randomized_quicksort(nums, l, mid - 1)
-        self.randomized_quicksort(nums, mid + 1, r)
+        self.randomized_partition(nums, l, mid - 1)
+        self.randomized_partition(nums, mid + 1, r)
 
     def sortArray(self, nums: List[int]) -> List[int]:
         self.randomized_quicksort(nums, 0, len(nums) - 1)
