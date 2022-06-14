0af54c7b8998c5a20a27e351a01c7b3ccb0ab61c
aizigao <weixiangzhe@live.com>
2022-06-14 11:08:46 +0800

    feat: sync

--- algorithm/leetCode_xx/215.数组中的第k个最大元素.py
+++ algorithm/leetCode_xx/215.数组中的第k个最大元素.py
@@ -1,4 +1,3 @@
-
 # @lc app=leetcode.cn id=215 lang=python3
 #
 # [215] 数组中的第K个最大元素
@@ -11,9 +10,38 @@ class Solution:
     # 方法一：基于快速排序的选择方法 O(nlogn)
     # 时间复杂度：O(n) 空间 O(logn)
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pass
+        l = 0
+        r = len(nums) - 1
+
+        # 转化第k大元素
+        k = len(nums) - k
+
+        while l <= r:
+            mid = self.partition(nums, l, r)
+            if mid < k:
+                l = mid + 1
+            elif mid > k:
+                r = mid - 1
+            else:
+                return nums[mid]
+        return -1
+
+    def partition(self, nums, l, r):
+        pivot = random.randint(l, r)
+        nums[pivot], nums[r] = nums[r], nums[pivot]
+        i = l - 1
+        for j in range(l, r):
+            if nums[j] < nums[r]:
+                i += 1
+                nums[j], nums[i] = nums[i], nums[j]
+        i += 1
+        nums[i], nums[r] = nums[r], nums[i]
+        return i
 
     # 二，堆排序
-    # TODO: 明天再看，这个需要小项堆的知识，再说吧
-    # 时间复杂度：O(n \log n)，建堆的时间代价是 O(n)O(n)，删除的总代价是 O(k \log n)O(klogn)，因为 k < nk<n，故渐进时间复杂为 O(n + k \log n) = O(n \log n)O(n+klogn)=O(nlogn)。
+    # 小项堆 binheap
+    def findKthLargest2(self, nums: List[int], k: int) -> int:
+        pass
+
+
 # @lc code=end
