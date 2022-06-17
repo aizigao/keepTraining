6b58207df030bc2146ce482dd7e08119dc382d58
aizigao <weixiangzhe@live.com>
2022-06-17 15:16:35 +0800

    sync

--- algorithm/leetCode_xx/83.删除排序链表中的重复元素.py
+++ algorithm/leetCode_xx/83.删除排序链表中的重复元素.py
@@ -14,8 +14,11 @@
 
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return
         slow = fast = head
 
+        #像 Java/Python 这类带有垃圾回收的语言，可以帮我们自动找到并回收这些「悬空」的链表节点的内存，而像 C++ 这类语言没有自动垃圾回收的机制，确实需要我们编写代码时手动释放掉这些节点的内存。
         while fast:
             if slow.val != fast.val:
                 slow.next = fast
@@ -23,8 +26,7 @@ class Solution:
             fast = fast.next
 
         # 多余的移除
-        if slow:
-            slow.next = None
+        slow.next = None
 
         return head
 
