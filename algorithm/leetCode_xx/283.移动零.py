5aa90300bab8deaa210ac638b1de96625bb69c50
aizigao <weixiangzhe@live.com>
2022-06-17 15:35:16 +0800

    sync

--- algorithm/leetCode_xx/283.移动零.py
+++ algorithm/leetCode_xx/283.移动零.py
@@ -9,19 +9,14 @@
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         n = len(nums)
+        slow = fast = 0
 
-        l, r = 0, 0
-
-        while r < n:
-            while nums[r] != 0:
-                nums[r], nums[l] = nums[l], nums[r]
-                l += 1
-
-            r += 1
+        while fast < n:
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
         return nums
 
 
