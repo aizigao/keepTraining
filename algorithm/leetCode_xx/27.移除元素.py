d67e1cb72623866a85911cdb5a5ca5e87d820345
aizigao <weixiangzhe@live.com>
2022-06-17 15:29:42 +0800

    sync

--- algorithm/leetCode_xx/27.移除元素.py
+++ algorithm/leetCode_xx/27.移除元素.py
@@ -4,9 +4,18 @@
 # [27] 移除元素
 #
 
+
 # @lc code=start
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        pass
-# @lc code=end
+        slow = fast = 0
 
+        while fast < len(nums):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
+
+
+# @lc code=end
