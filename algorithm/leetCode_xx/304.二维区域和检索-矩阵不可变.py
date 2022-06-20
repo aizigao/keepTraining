05789440156940822c6c3d0b27038e48782e21ad
aizigao <weixiangzhe@live.com>
2022-06-20 14:55:28 +0800

    sync

--- algorithm/leetCode_xx/304.二维区域和检索-矩阵不可变.py
+++ algorithm/leetCode_xx/304.二维区域和检索-矩阵不可变.py
@@ -9,25 +9,21 @@
 class NumMatrix:
     # 初始化时 完成 前缀和
     def __init__(self, matrix: List[List[int]]):
-        r = len(matrix) + 1
-        c = len(matrix[0]) + 1
-        preSum = [[0 for j in range(c)] for i in range(r)]
+        rows = len(matrix)
+        columns = len(matrix[0])
 
+        # 计算前缀和
+        preSum = [[0 for c in range(columns + 1)] for r in range(rows + 1)]
+        for r in range(1, rows + 1):
+            for c in range(1, columns + 1):
+                preSum[r][c] = preSum[r - 1][c] + preSum[r][c - 1] + matrix[
+                    r - 1][c - 1] - preSum[r - 1][c - 1]
         self.preSum = preSum
-        for i in range(1, r):
-            for j in range(1, c):
-                #  计算每个矩阵 [0, 0, i, j] 的元素和
-                preSum[i][j] =  preSum[i    ][j - 1] + \
-                                preSum[i - 1][j    ] + \
-                                matrix[i - 1][j - 1] - \
-                                preSum[i - 1][j - 1]
 
     def sumRegion(self, x1: int, y1: int, x2: int, y2: int) -> int:
         preSum = self.preSum
-        return preSum[x2 + 1][y2 + 1] \
-             - preSum[x1][y2 + 1] \
-             - preSum[x2 + 1][y1] \
-             + preSum[x1][y1]
+        return preSum[x2 + 1][y2 + 1] - preSum[x2 + 1][y1] - preSum[x1][
+            y2 + 1] + preSum[x1][y1]
 
 
 # Your NumMatrix object will be instantiated and called as such:
