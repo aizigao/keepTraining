44c090d400b9895422df239b88ea4216451adb02
aizigao <weixiangzhe@live.com>
2022-06-20 17:23:38 +0800

    sync

--- algorithm/leetCode_xx/54.螺旋矩阵.py
+++ algorithm/leetCode_xx/54.螺旋矩阵.py
@@ -8,41 +8,39 @@
 # @lc code=start
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        m = len(matrix)
-        n = len(matrix[0])
-
-        upper_bound = 0
-        lower_bound = m - 1
-        left_bound = 0
-        right_bound = n - 1
+        rows = len(matrix)
+        columns = len(matrix[0])
         res = []
 
-        # len(res) === m*n 遍历完成
+        l = 0
+        u = 0
+        b = rows - 1
+        r = columns - 1
 
-        while len(res) < m * n:
-            # 在项部 left to right
-            if upper_bound <= lower_bound:
-                for j in range(left_bound, right_bound + 1):
-                    res.append(matrix[upper_bound][j])
-                upper_bound += 1
+        while len(res) < columns * rows:
+            # 在项部 从左到右
+            if u <= b:
+                for j in range(l, r + 1):
+                    res.append(matrix[u][j])
+                u += 1
 
-            # 在右侧 up to bottom
-            if left_bound <= right_bound:
-                for i in range(upper_bound, lower_bound + 1):
-                    res.append(matrix[i][right_bound])
-                right_bound -= 1
+            # 在右侧 从上到下
+            if l <= r:
+                for i in range(u, b + 1):
+                    res.append(matrix[i][r])
+                r -= 1
 
-            # 在底部从右向左遍历
-            if upper_bound <= lower_bound:
-                for j in range(right_bound, left_bound - 1, -1):
-                    res.append(matrix[lower_bound][j])
-                lower_bound -= 1
+            # 在下侧 从右到左
+            if u <= b:
+                for j in range(r, l - 1, -1):
+                    res.append(matrix[b][j])
+                b -= 1
 
-            # 在左侧 bototm to up
-            if left_bound <= right_bound:
-                for i in range(lower_bound, upper_bound - 1, -1):
-                    res.append(matrix[i][left_bound])
-                left_bound += 1
+            # 在左侧 从下到上
+            if l <= r:
+                for i in range(b, u - 1, -1):
+                    res.append(matrix[i][l])
+                l += 1
         return res
 
 
