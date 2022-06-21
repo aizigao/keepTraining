6f93a0ab4bab23cb2d80956d9c28e622643a6471
aizigao <weixiangzhe@live.com>
2022-06-21 15:36:03 +0800

    feat: sync

--- algorithm/leetCode_xx/3.无重复字符的最长子串.py
+++ algorithm/leetCode_xx/3.无重复字符的最长子串.py
@@ -18,11 +18,11 @@ l, r = 0
 
 while r < len(s):
     # 增大窗口
-    window.append(s[r])
+    window.add(s[r])
     r+=1
     while (windowNeedsShrink):
         # 缩小窗口
-        window.pop(0)
+        window.remove(s[l])
         l+=1
 '''
 
@@ -30,39 +30,40 @@ while r < len(s):
 
 
 class Solution:
+    def lengthOfLongestSubstring1(self, s: str) -> int:
+        # 哈希集合，记录每个字符是否出现过
+        occ = set()
+        n = len(s)
+        # 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        rk, ans = -1, 0
+        for i in range(n):
+            if i != 0:
+                # 左指针向右移动一格，移除一个字符
+                occ.remove(s[i - 1])
+            while rk + 1 < n and s[rk + 1] not in occ:
+                # 不断地移动右指针
+                occ.add(s[rk + 1])
+                rk += 1
+            # 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = max(ans, rk - i + 1)
+        return ans
+
     def lengthOfLongestSubstring(self, s: str) -> int:
         win = dict()
+        l = r = 0
         rst = 0
-        l, r = 0, 0
 
         while r < len(s):
-            char = s[r]
+            ch = s[r]
             r += 1
-            # 更新数据
-            win[char] = 1 if char not in win else win[char] + 1
+            win[ch] = win[ch] + 1 if ch in win else 1
 
-            # 收缩
-            while char in win and win[char] > 1:
+            while ch in win and win[ch] > 1:
                 d = s[l]
-                l += 1
-                win[d] -= 1
+                win.remove(d)
+                l -= 1
             rst = max(rst, r - l)
-
         return rst
-
-        # occ = set()
-        # n = len(s)
-        # rk = -1
-        # ans = 0
-
-        # for i in range(n):
-        #     if i != 0:
-        #         occ.remove(s[i - 1])
-        #     while rk + 1 < n and s[rk + 1] not in occ:
-        #         occ.add(s[rk + 1])
-        #         rk += 1
-        #     ans = max(ans, rk - i + 1)
-        # return ans
 
 
 # @lc code=end
--- algorithm/leetCode_xx/76.最小覆盖子串.py
+++ algorithm/leetCode_xx/76.最小覆盖子串.py
@@ -11,30 +11,55 @@ import collections
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # hash
-        cnt = collections.Counter(t)
-        #  need = 0 为匹配到
-        need = len(t)
+        if len(s) < len(t):
+            return ''
 
-        n = len(s)
-        start, end = 0, -1
+        # 「窗口」中的相应字符的出现次
+        window = dict()
+        #  t 中字符出现次数
+        need = collections.Counter([i for i in t])
 
-        # 初始化为一个不可能的较大值
-        min_len = n + 1
+        # 记录最小覆盖子串的起始索引及长度
+        start = 0
+        size = float('inf')
 
-        left, right = 0, 0
+        left = right = 0
 
-        for right in range(n):
-            ch = s[right]
+        # 满足 need 条件的字符个数
+        # valid 和 need.size 的大小相同，则说明窗口已满足条件
+        valid = 0
 
-            if ch in cnt:
-                if cnt[ch] > 0:
-                    need -= 1
-                cnt[ch] -= 1
-            while need == 0:
-                if right - left + 1 < min_len:
-                    min_len = right - left + 1
-                    start, end = left, right
+        while right < len(s):
+            # 开始滑动
+
+            c = s[right]
+            # 扩大窗口
+            right += 1
+
+            # 更新窗口数据
+            if c in need:
+                window[c] = window[c] + 1 if c in window else 1
+
+                if window[c] == need[c]:
+                    valid += 1
+
+            # 满足后 开始缩小窗口
+
+            while valid == len(need):
+                # 更新 结构
+                if right - left < size:
+                    start = left
+                    size = right - left
+                # 缩小
+                d = s[left]
+                left += 1
+
+                # 更新窗口数据
+                if d in need:
+                    if window[d] == need[d]:
+                        valid -= 1
+                    window[d] -= 1
+        return '' if size == float('inf') else s[start:start + size]
 
 
 # @lc code=end
