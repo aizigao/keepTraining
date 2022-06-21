7ba294be6539690237c988235153cd61c75c493a
aizigao <weixiangzhe@live.com>
2022-06-21 19:04:09 +0800

    sync

--- algorithm/Structures_python/6_sorting_searching/binarySearch.py
+++ algorithm/Structures_python/6_sorting_searching/binarySearch.py
@@ -17,6 +17,37 @@ def binarySearch(alist, item):
     return found
 
 
-testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42, ]
+testlist = [
+    0,
+    1,
+    2,
+    8,
+    13,
+    17,
+    19,
+    32,
+    42,
+]
 print(binarySearch(testlist, 3))
 print(binarySearch(testlist, 13))
+
+
+def binSearch2(nums, target):
+    l = 0
+    r = len(nums) - 1
+
+    while l <= r:
+        # 防止了 left 和 right 太大，直接相加导致溢出的情况。
+        mid = l + (r - l) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            r = mid - 1
+        elif nums[mid] < target:
+            l = mid + 1
+
+    return -1
+
+
+print('x2', binSearch2(testlist, 13))
