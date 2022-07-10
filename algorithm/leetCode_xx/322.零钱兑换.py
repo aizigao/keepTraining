4a8f15b4724d78bbd2d1e35fccc01c02e797b3c0
aizigao <weixiangzhe@live.com>
2022-07-10 14:05:18 +0800

    sync

--- algorithm/leetCode_xx/322.零钱兑换.py
+++ algorithm/leetCode_xx/322.零钱兑换.py
@@ -8,8 +8,36 @@
 
 
 class Solution:
+    '''
+    暴力
+    '''
+    def coinChange_0(self, coins, amount):
+        # // base case
+        if amount == 0:
+            return 0
+        if amount < 0:
+            return -1
+
+        res = float('inf')
+
+        for coin in coins:
+            subP = self.coinChange(coins, amount - coin)
+            # 如果无解
+            if subP == -1:
+                continue
+            # 做选择
+            res = min(res, subP + 1)
+        return res if res != float('inf') else -1
+
     # 方法一：记忆化搜索
     # F(S)=F(S−C)+1
+    '''
+    长度为 k
+    本身复杂度 O(k)
+    调用次数: 等于状态数 O(n)
+    O(kn) --> O(n)
+    '''
+
     def coinChange_1(self, coins, amount):
         @functools.lru_cache(amount)
         def dp(rem):
@@ -30,12 +58,15 @@ class Solution:
 
     def coinChange(self, coins, amount):
         # 新建一个长度为 amount+1的数组
-        dp = [float('inf')] * (amount+1)
+        dp = [float('inf')] * (amount + 1)
         dp[0] = 0
 
+        # 便利所有状态
         for coin in coins:
-            for x in range(coin, amount+1):
-                dp[x] = min(dp[x], dp[x-coin] + 1)
+            # 求选择的最小值
+            # i - coin < 0 无解跳过
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
         return dp[amount] if dp[amount] != float('inf') else -1
 
 
