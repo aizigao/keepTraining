479280a6c32ba0796a632a87733eccbbbd083bda
aizigao <weixiangzhe@live.com>
2022-07-15 14:40:45 +0800

    sync

---
+++ algorithm/leetCode_xx/931.下降路径最小和.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=931 lang=python3
+#
+# [931] 下降路径最小和
+#
+
+
+# @lc code=start
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        pass
+
+
+# @lc code=end
