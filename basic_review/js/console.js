b0c8341862308b2e1612ece4c6c5f929befd35b7
aizigao <weixiangzhe@live.com>
2024-08-14 09:49:05 +0800

    feat: sync

---
+++ basic_review/js/console.js
@@ -0,0 +1,6 @@
+console.info('The total weight of %i %s and %d %s is %f grams.', 3, 'apples', 2, 'oranges', 432.4);
+
+const style = 'background-color: darkblue; color: white; font-style: italic; border: 5px solid hotpink; font-size: 2em;'
+console.log("%cHooray", style);
+
+console.log('\x1B[41;93;4mHello\x1B[m');
